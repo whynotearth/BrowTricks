b3b26f069961d7a30ee55678ac26097a0ee58118
spark <parkyngj@gmail.com>
2020-04-21 16:11:39 -0700

    feat: extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#7ACCEA',
+        secondary: '#ABEDFC',
+        background: '#F4F1F1',
+        surface: '#F4F1F1',
+        action: '#0C6986',
+        error: '#C04545',
+        'on-primary': '#0C6986',
+        'on-primary-white': '#FFFFFF',
+        'on-secondary': '#0C6986',
+        'on-background': '#000000',
+        'on-surface': '#000000'
+      }
+    }
   },
   variants: {},
   plugins: []
