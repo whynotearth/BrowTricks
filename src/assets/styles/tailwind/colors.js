56cde0d7a3041337ca8b2d5f60f74fb5f055710b
spark <parkyngj@gmail.com>
2020-04-22 16:09:06 -0700

    chore: reorganize existing styles for colors, opacities, shadows

---
+++ src/assets/styles/tailwind/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#7ACCEA',
+  secondary: '#ABEDFC',
+  background: '#F4F1F1',
+  surface: '#F4F1F1',
+  action: '#0C6986',
+  error: '#C04545',
+  'on-primary': '#0C6986',
+  'on-primary-white': '#FFFFFF',
+  'on-secondary': '#0C6986',
+  'on-background': '#000000',
+  'on-surface': '#000000'
+};
+
+module.exports = colors;
