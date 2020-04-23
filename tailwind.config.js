d0594b177e452c9c5a28794e80ec91e4378498ec
spark <parkyngj@gmail.com>
2020-04-22 19:27:01 -0700

    chore: register roboto as default sans font and use sans class in app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const boxShadow = require('./src/assets/styles/tailwind/shadows');
 const colors = require('./src/assets/styles/tailwind/colors');
 const opacity = require('./src/assets/styles/tailwind/opacities');
@@ -8,6 +10,9 @@ module.exports = {
       boxShadow,
       colors,
       opacity
+    },
+    fontFamily: {
+      'sans': ['Roboto', ...defaultTheme.fontFamily.sans]
     }
   },
   variants: {},
