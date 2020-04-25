b8b4f396ba87ad63d5ad367f6e0faa0a9080a7c1
spark <parkyngj@gmail.com>
2020-04-25 10:14:23 -0700

    feat: add type to make button base tag configurable

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,10 @@
 <template>
-  <div
+  <component
+    :is="tag"
     class="full-width rounded tg-color-label-mobile py-2 px-3 flex items-center justify-center"
+    @click="$emit('clicked')"
+    :href="href"
+    :to="to"
   >
     <div class="text-left">
       <component
@@ -13,13 +17,19 @@
     <div class="flex-grow">
       {{ ctaText }}
     </div>
-  </div>
+  </component>
 </template>
 
 <script>
 export default {
   name: 'Button',
   props: {
+    href: {
+      type: String
+    },
+    to: {
+      type: String
+    },
     ctaText: {
       type: String,
       required: true
@@ -29,6 +39,17 @@ export default {
     },
     iconStyle: {
       type: Array
+    }
+  },
+  computed: {
+    tag() {
+      if (this.href) {
+        return 'a';
+      } else if (this.to) {
+        return 'router-link';
+      } else {
+        return 'div';
+      }
     }
   }
 };
