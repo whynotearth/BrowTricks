045a2dc562aaf1baf87091bdc2f2ee10c2bfd706
spark <parkyngj@gmail.com>
2020-04-27 12:09:35 -0700

    feat: add basic menu header component

---
+++ src/components/MenuHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-14 flex items-center p-5 fixed top-0 z-10">
+    <MenuIcon class="text-on-primary-white fill-current w-6 h-6" />
+  </div>
+</template>
+
+<script>
+import MenuIcon from '@/assets/icons/menu.svg';
+
+export default {
+  name: 'MenuHeader',
+  components: {
+    MenuIcon
+  }
+};
+</script>
+
+<style scoped />
