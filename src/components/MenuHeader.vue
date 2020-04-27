ee81390418ee0fea9388cba272cdc888a8d607ec
spark <parkyngj@gmail.com>
2020-04-27 15:07:36 -0700

    feat: allow MenuHeader component to take tenantName and display

--- src/components/MenuHeader.vue
+++ src/components/MenuHeader.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="h-14 flex items-center p-5 fixed top-0 z-10">
-    <MenuIcon class="text-on-primary-white fill-current w-6 h-6" />
+  <div
+    class="h-14 flex items-center p-5 fixed top-0 z-10 text-on-background-image-high"
+  >
+    <MenuIcon class="fill-current w-6 h-6" />
+    <div v-if="tenantName" class="tg-h2-mobile pl-4">
+      {{ tenantName }}
+    </div>
   </div>
 </template>
 
@@ -11,6 +16,9 @@ export default {
   name: 'MenuHeader',
   components: {
     MenuIcon
+  },
+  props: {
+    tenantName: String
   }
 };
 </script>
