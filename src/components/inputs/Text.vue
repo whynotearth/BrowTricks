c43648d274aa30d4c15a644bf6c72a488d9145cb
spark <parkyngj@gmail.com>
2020-05-02 23:00:17 -0700

    feat: text input component more configurable, type and styling props

--- src/components/inputs/Text.vue
+++ src/components/inputs/Text.vue
@@ -2,9 +2,10 @@
   <div class="relative tg-body-mobile">
     <input
       :id="idName"
-      type="text"
+      :type="type"
       placeholder=" "
-      class="input border-b border-current w-full appearance-none outline-none bg-transparent"
+      class="input border-b w-full appearance-none outline-none bg-transparent"
+      :class="inputStyle"
     />
     <label
       :for="idName"
@@ -13,7 +14,8 @@
       {{ label }}
     </label>
     <div
-      class="line border-t border-current w-full h-px transition-all duration-200"
+      class="line border-t w-full h-px transition-all duration-200"
+      :lineStyle="lineStyle"
     />
   </div>
 </template>
@@ -22,13 +24,23 @@
 export default {
   name: 'TextInput',
   props: {
+    idName: {
+      type: String,
+      required: true
+    },
     label: {
       type: String,
       required: true
     },
-    idName: {
+    type: {
       type: String,
-      required: true
+      default: 'text'
+    },
+    inputStyle: {
+      type: Array
+    },
+    lineStyle: {
+      type: Array
     }
   }
 };
