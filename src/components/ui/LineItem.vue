f32141ee298782b06f9ad10ed1b820f34924d698
spark <parkyngj@gmail.com>
2020-05-04 23:49:55 -0700

    feat: add draft line item component

---
+++ src/components/ui/LineItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex items-center w-full">
+    <img src="https://dummyimage.com/40x40/000/fff" class="h-10 w-10 mr-2" />
+    <div class="tg-body-mobile text-left">
+      <p class="text-on-background">Paul Chris Luke</p>
+      <p class="text-on-background text-opacity-medium">Microblading</p>
+      <p class="text-on-background text-opacity-medium">
+        Feb 12, 2020 - 5:00PM
+      </p>
+    </div>
+    <div class="flex-grow text-right">
+      <p class="text-on-background text-opacity-medium tg-caption-mobile">
+        $100
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LineItem'
+};
+</script>
+
+<style scoped />
