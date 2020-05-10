dc8dfab305a6c365f0e16d6c81caeb6f106f6112
spark <parkyngj@gmail.com>
2020-05-09 23:11:04 -0700

    feat: MenuHeader component -> more modular Header component

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="h-14 w-full p-5 sticky top-0 z-10 flex items-center">
+    <div @click="$emit('iconClicked')">
+      <slot name="icon" />
+    </div>
+    <div class="tg-h2-mobile">
+      <slot name="content" />
+    </div>
+    <div class="flex-end flex items-center">
+      <div @click="$emit('detailPrimaryClicked')">
+        <slot name="detail-primary" />
+      </div>
+      <div @click="$emit('detailSecondaryClicked')">
+        <slot name="detail-secondary" />
+      </div>
+    </div>
+
+    <slot name="menu-drawer" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuHeader'
+};
+</script>
