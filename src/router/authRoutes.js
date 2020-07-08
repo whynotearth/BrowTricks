799dbfbfb8a08eeac87b99eaae75feabf32b1ba3
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-08 11:26:21 +0300

    fix: auth routes edited

--- src/router/authRoutes.js
+++ src/router/authRoutes.js
@@ -6,22 +6,9 @@ export const authRoutes = [
     meta: { layout: () => import('@/layouts/TenantLayout.vue') }
   },
   {
-    path: '/auth/sign-up/:step',
-    name: 'SignUp',
-    props: true,
-    component: () => import('@/views/AuthSignUp.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
     path: '/auth/login',
     name: 'LogIn',
     component: () => import('@/views/AuthLogIn.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
-    path: '/auth/sign-up/success/:slug',
-    name: 'SignUpSuccess',
-    component: () => import('@/components/auth/SignUpSuccess.vue'),
     meta: { layout: () => import('@/layouts/TenantLayout.vue') }
   }
 ];
