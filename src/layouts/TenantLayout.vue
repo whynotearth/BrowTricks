2305ed3de9489a399a4241827855e5fa15426bbe
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-09 09:50:04 +0300

    chore: baseappheader component removed

--- src/layouts/TenantLayout.vue
+++ src/layouts/TenantLayout.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="min-h-screen mx-auto">
-    <BaseAppBarHeader
-      v-if="appBar"
-      :title="appBar.title"
-      :toLink="appBar.backRoute"
-    >
-      <template #menu v-if="appBar.newItem">
-        <div class="flex-grow inline-block text-right">
-          <router-link :to="appBar.newItem" class="relative">
-            <NewItemIcon class="float-right w-5 h-5 my-auto cursor-pointer" />
-          </router-link>
-        </div>
-      </template>
-    </BaseAppBarHeader>
     <transition name="fade" mode="out-in">
       <router-view class="container mx-auto"></router-view>
     </transition>
@@ -20,13 +7,11 @@
 </template>
 
 <script>
-import BaseAppBarHeader from '@/components/BaseAppBarHeader';
 import NewItemIcon from '@/assets/icons/new_item.svg';
 
 export default {
   name: 'TenantLayout',
   components: {
-    BaseAppBarHeader,
     NewItemIcon
   },
   computed: {
