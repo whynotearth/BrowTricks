d755bc0ad9185f54570116784b300941fc7afe2d
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-07-13 23:16:09 +0430

    update slider

--- src/views/PMUFormSetup.vue
+++ src/views/PMUFormSetup.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <BaseSlider>
-      <div class="keen-slider__slide">1</div>
-      <div class="keen-slider__slide">2</div>
-      <div class="keen-slider__slide"><img src="http://placehold.it/500x600" alt=""></div>
-      <div class="keen-slider__slide"><img src="http://placehold.it/500x200" alt=""></div>
-      <div class="keen-slider__slide">5</div>
-      <div class="keen-slider__slide">6</div>
-    </BaseSlider>
+    <div class="max-w-md mx-auto">
+      <BaseSlider>
+        <div class="keen-slider__slide">1</div>
+        <div class="keen-slider__slide">
+          <img src="http://placehold.it/500x600" alt="" />
+        </div>
+        <div class="keen-slider__slide">
+          <img src="http://placehold.it/500x200" alt="" />
+        </div>
+        <div class="keen-slider__slide">4</div>
+      </BaseSlider>
+    </div>
   </div>
 </template>
 
