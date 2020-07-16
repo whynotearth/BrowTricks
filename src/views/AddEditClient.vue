e8e8a7f06f54dffa5ff1ffc1717978a5f6dc76f0
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-16 13:10:29 +0300

    add client changed to stepper

--- src/views/AddEditClient.vue
+++ src/views/AddEditClient.vue
@@ -1,185 +1,165 @@
 <template>
-  <div class="bg-surface text-left min-h-screen">
-    <BaseHeader
-      slot="header"
-      class="bg-footer text-white"
-      @iconClicked="
-        $router.push({ name: 'ClientList', params: { tenantSlug } })
-      "
+  <div class="text-left">
+    <div
+      class="min-h-screen bg-background w-full flex flex-col justify-between"
     >
-      <ArrowBack slot="icon" class="h-6 w-6 fill-current" />
-      <span slot="content" class="pl-5">Add Client</span>
-    </BaseHeader>
-    <div class="m-4 bg-white shadow-1dp py-4 px-2 rounded-lg">
-      <material-input
-        v-model="$v.firstName.$model"
-        label="First Name"
-        labelBg="bg-white"
-        :error="$v.firstName.$dirty && !$v.firstName.required"
-      >
-        <span
-          v-if="$v.firstName.$dirty && !$v.firstName.required"
-          class="text-red-600 text-xs"
-        >
-          First Name is required
-        </span>
-      </material-input>
-      <material-input
-        v-model="$v.lastName.$model"
-        label="Last Name"
-        labelBg="bg-white"
-        :error="$v.lastName.$dirty && !$v.lastName.required"
-      >
-        <span
-          v-if="$v.lastName.$dirty && !$v.lastName.required"
-          class="text-red-600 text-xs"
-        >
-          Last Name is required
-        </span>
-      </material-input>
-      <material-input
-        v-model="$v.phone.$model"
-        label="Phone Number"
-        labelBg="bg-white"
-        :error="$v.phone.$dirty && (!$v.phone.required || !$v.phone.minLength)"
-      >
-        <span
-          v-if="$v.phone.$dirty && !$v.phone.required"
-          class="text-red-600 text-xs"
-        >
-          Phone number is required
-        </span>
-        <span v-if="!$v.phone.minLength" class="text-red-600 text-xs">
-          Please enter a valid phone number
-        </span>
-      </material-input>
-      <material-input
-        v-model="$v.email.$model"
-        label="Email"
-        labelBg="bg-white"
-        :error="$v.email.$dirty && (!$v.email.required || !$v.email.minLength)"
-      >
-        <span
-          v-if="$v.email.$dirty && !$v.email.required"
-          class="text-red-600 text-xs"
-        >
-          Email is required
-        </span>
-        <span
-          v-if="$v.email.$dirty && !$v.email.email"
-          class="text-red-600 text-xs"
-        >
-          Please enter a email address
-        </span>
-      </material-input>
-    </div>
-    <ExpansionPanel
-      title="Notification Settings"
-      @click="$router.push({ name: 'Notifications' })"
-    >
-      <Notification slot="preIcon" class="h-6 w-6 fill-current" />
-    </ExpansionPanel>
-    <hr class="mt-12 mb-8" />
-    <ImageUpload v-model="images" :defaultImages="[]">
-      <template #title>
-        <div class="tg-body-mobile ">
-          <span class="text-on-background text-opacity-high"> Image </span>
-          <span class="text-on-background text-opacity-medium">
-            ( 500 x 599 pixels JPEG / PNG )
-          </span>
+      <div>
+        <StepperTop :navigation="navigation" :page="page" />
+        <div class="my-4">
+          <transition name="fade" mode="out-in">
+            <keep-alive>
+              <component
+                :is="component"
+                :ref="component"
+                @nextStep="nextStep"
+              ></component>
+            </keep-alive>
+          </transition>
+          <div v-if="errors" class="px-4 text-error text-xs">
+            <div v-for="(error, key) in errors" :key="key">
+              <p v-for="(detail, key) in error" :key="key">{{ detail }}</p>
+            </div>
+          </div>
         </div>
-      </template>
-    </ImageUpload>
-    <hr class="my-4" />
-    <ImageUpload id="files" v-model="files" :defaultImages="[]">
-      <template #title>
-        <div class="tg-body-mobile ">
-          <span class="text-on-background text-opacity-high"> Files </span>
-        </div>
-      </template>
-    </ImageUpload>
-    <hr class="mt-12 mb-8" />
-    <ExpansionPanel
-      @click="$router.push({ name: 'PMU' })"
-      title="PMU"
-      middleText="Incomplete"
-    >
-      <Document slot="preIcon" class="h-6 w-6 fill-current" />
-    </ExpansionPanel>
-    <ExpansionPanel @click="$router.push({ name: 'Notes' })" title="Notes">
-      <Notes slot="preIcon" class="h-6 w-6 fill-current" />
-    </ExpansionPanel>
-    <div class="mt-4 mx-4 py-6 px-2">
-      <Button
-        class="rounded-full"
-        title="Save"
-        :isRipple="false"
-        @clicked="save"
+      </div>
+      <StepperBottom
+        :page="page"
+        :nextStepText="
+          `${
+            navigation[page] && page < navigation.length
+              ? 'NEXT STEP'
+              : 'FINISH'
+          } ►`
+        "
+        @previousStep="previousStep"
+        @nextStep="nextStep"
+        :firstPageStepBack="true"
       />
     </div>
   </div>
 </template>
 
 <script>
-import { required, minLength, email } from 'vuelidate/lib/validators';
-import BaseHeader from '@/components/BaseHeader.vue';
-import MaterialInput from '@/components/inputs/MaterialInput.vue';
-import ImageUpload from '@/components/imageUpload/ImageUpload.vue';
-import Button from '@/components/Button.vue';
-import ExpansionPanel from '@/components/ExpansionPanel.vue';
-
-import ArrowBack from '@/assets/icons/arrow_back.svg';
-import Document from '@/assets/icons/document.svg';
-import Notes from '@/assets/icons/notes.svg';
-import Notification from '@/assets/icons/notification.svg';
+import { mapMutations, mapActions, mapState } from 'vuex';
+import StepperTop from '@/components/BaseStepperTopBar';
+import StepperBottom from '@/components/BaseStepperBottomBar';
+import BasicInfo from '@/components/client/BasicInfo';
+import Notifications from '@/components/client/Notifications';
+import LinkAccount from '@/components/tenant/LinkAccount';
 
 export default {
   name: 'AddEditClient',
+  components: {
+    StepperTop,
+    StepperBottom,
+    BasicInfo,
+    Notifications,
+    LinkAccount
+  },
   props: {
     tenantSlug: {
       type: String,
       required: true
     }
   },
-  components: {
-    BaseHeader,
-    ArrowBack,
-    MaterialInput,
-    ImageUpload,
-    Button,
-    Document,
-    Notes,
-    Notification,
-    ExpansionPanel
-  },
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      phone: '',
-      email: '',
-      images: [],
-      files: []
+      navigation: [
+        {
+          step: 'basic-info',
+          name: 'Basic Info'
+        },
+        {
+          step: 'notifications',
+          name: 'Notifications'
+        }
+      ],
+      errors: null
     };
   },
-  validations: {
-    firstName: {
-      required
-    },
-    lastName: {
-      required
-    },
-    phone: {
-      required,
-      minLength: minLength(7)
-    },
-    email: {
-      required,
-      email
+  computed: {
+    ...mapState('client', ['page']),
+    component() {
+      return this.navigation[this.page - 1].step;
     }
   },
   methods: {
-    save() {}
+    ...mapMutations('client', [
+      'pageChange',
+      'resetCreateClientForm'
+    ]),
+    ...mapActions('client', ['createClient']),
+    ...mapActions('auth', ['ping']),
+    previousStep() {
+      if (this.page > 1) {
+        this.pageChange(this.page - 1);
+      } else if (this.page === 1) {
+        this.goClientList();
+      }
+    },
+    nextStep() {
+      let valid = true;
+      const isThereValidationAtComponent = !!this.$refs[this.component].$v;
+      if (isThereValidationAtComponent) {
+        this.$refs[this.component].$v.$touch();
+        valid = !this.$refs[this.component].$v.$invalid;
+      }
+
+      if (valid) {
+        if (this.page < this.navigation.length) {
+          this.pageChange(this.page + 1);
+        } else {
+          this.register();
+        }
+      }
+    },
+    register() {
+      this.createClient({
+            tenantSlug: this.tenantSlug
+          })
+        .then(() => {
+          this.goClientList();
+        })
+        .catch(error => {
+          if (error.response.status === 401) {
+            const isLinkAccountAvailabble = this.navigation.findIndex(
+              nav => nav.step === 'link-account'
+            );
+            if (isLinkAccountAvailabble !== -1) {
+              this.navigation.push(
+                {
+                  step: 'link-account',
+                  name: 'Link Account'
+                }
+              );
+            }
+          }
+          this.errors = error.response.data.errors;
+        });
+    },
+    goClientList() {
+      this.$router.push({ name: 'ClientList', params: { tenantSlug: this.tenantSlug } });
+    }
+  },
+  watch: {
+    component(step) {
+      this.$router.push(this.$route.params.clientId ? { name: 'EditClient', params: { step, tenantSlug: this.tenantSlug, clientId: this.$route.params.clientId } } : { name: 'AddClient', params: { step, tenantSlug: this.tenantSlug } }).catch(() => {});
+    },
+    '$route.params.step': {
+      immediate: true,
+      handler() {
+        const stepFromUrl = this.$route.params.step;
+        const index = this.navigation.findIndex(
+          nav => nav.step === stepFromUrl
+        );
+
+        if (index >= 0) {
+          this.pageChange(index + 1);
+        } else if (index < 0) {
+          this.goClientList();
+        }
+      }
+    }
   }
 };
 </script>
