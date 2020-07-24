4d955ad35c253282f9d40991af314dee6b757086
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-24 10:49:42 +0300

    feat: client notifications - route created

--- src/router/index.js
+++ src/router/index.js
@@ -68,6 +68,15 @@ const routes = [
       requiresAuth: true
     }
   },
+  {
+    path: '/tenant/:tenantSlug/clients/edit/:clientId/notifications',
+    name: 'ClientNotifications',
+    props: true,
+    component: () => import('@/views/ClientNotifications.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
   ...authRoutes,
   {
     path: '/',
