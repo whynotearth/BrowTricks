e5bcef81087d72ac20a822a6f6f6e4c2ac125f86
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-08-05 12:53:43 +0430

    remove: PMU - questions flow

--- src/router/PMURoutes.js
+++ src/router/PMURoutes.js
@@ -5,23 +5,5 @@ export const PMURoutes = [
     component: () => import('@/views/PMUFormSetup.vue'),
     props: true,
     meta: { layout: () => import('@/layouts/WithTitleBarLayout.vue') }
-  },
-  {
-    // needs clientId?
-    path: '/tenant/:tenantSlug/client/:clientId/pmu/flow-methods',
-    name: 'PMUFlowMethods',
-    component: () => import('@/views/PMUFlowMethods.vue'),
-    meta: { layout: () => import('@/layouts/WithTitleBarLayout.vue') }
-  },
-  {
-    path: '/tenant/:tenantSlug/client/:clientId/pmu/questions-intro',
-    name: 'PMUFlowStart',
-    component: () => import('@/views/PMUFlowStart.vue')
-  },
-  {
-    path: '/tenant/:tenantSlug/client/:clientId/pmu/flow-questions/:stepSlug',
-    name: 'PMUFlowQuestions',
-    component: () => import('@/views/PMUFlowQuestions.vue'),
-    props: true
   }
 ];
