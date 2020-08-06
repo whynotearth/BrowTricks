d7fc0ff339f98493344bb5f3a50d09d30b977773
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-08-06 10:46:32 +0430

    layout - handle back button in pmu sign page

--- src/layouts/WithTitleBarLayout.vue
+++ src/layouts/WithTitleBarLayout.vue
@@ -5,7 +5,10 @@
     <div
       class="bg-newsurface z-20 shadow-4dp flex items-center p-4 sticky top-0"
     >
-      <IconBack class="text-white mr-2" />
+      <!-- icon -->
+      <a class="cursor-pointer" @click.prevent="iconClick">
+        <IconBack class="text-white mr-2" />
+      </a>
       <h1 class="tg-h2-mobile text-opacity-high text-white ml-2">
         {{ currentTitle }}
       </h1>
@@ -26,15 +29,32 @@ export default {
   components: { IconBack },
   data: () => ({
     isVisible: false,
-    currentTitle: ''
+    currentTitle: '',
+    backRoute: null
   }),
   mounted() {
     this.isVisible = true;
-    this.currentTitle = this.$route.meta.title;
-    console.log('this.title', this.currentTitle);
-    this.$on('layoutTitle', data => {
-      this.currentTitle = data;
-    });
+    this.handleBackRoute();
+    this.handleTitle();
+  },
+  methods: {
+    iconClick() {
+      this.$router.push(this.backRoute);
+    },
+    handleTitle() {
+      this.currentTitle = this.$route.meta.title;
+      // listen for title updates
+      this.$on('layoutTitle', data => {
+        this.currentTitle = data;
+      });
+    },
+    handleBackRoute() {
+      this.backRoute = this.$route.meta.backRoute;
+      // listen for back route updates
+      this.$on('layoutBackRoute', data => {
+        this.backRoute = data;
+      });
+    }
   }
 };
 </script>
