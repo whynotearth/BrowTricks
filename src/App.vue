28b8089aa1098747d941e5c66889180e75c2f607
giteshk11 <writemail2gitesh@gmail.com>
2020-08-18 16:50:04 +0530

    navigation transition added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="app" class="min-h-vh100 h-full text-center font-sans">
-    <component :is="this.$route.meta.layout || 'div'" class="h-full min-h-vh100">
-      <router-view />
-    </component>
+    <SlideBack>
+      <component
+        :is="this.$route.meta.layout || 'div'"
+        class="h-full min-h-vh100"
+      >
+        <SlideBack>
+          <router-view />
+        </SlideBack>
+      </component>
+    </SlideBack>
     <transition name="fade">
       <div
         v-if="overlayModel.title || overlayModel.message"
@@ -21,11 +28,12 @@
 import BaseOverlaySuccess from '@/components/BaseOverlaySuccess.vue';
 import { mapGetters } from 'vuex';
 import vhFix from '@/mixins/vhFix.js';
+import SlideBack from '@/components/animations/SlideBack.vue';
 
 export default {
   name: 'App',
   mixins: [vhFix],
-  components: { BaseOverlaySuccess },
+  components: { BaseOverlaySuccess, SlideBack },
   computed: {
     ...mapGetters('overlay', {
       overlayModel: 'model'
