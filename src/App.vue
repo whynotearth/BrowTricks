a203b20fbd26b00f28f29f7bf2aeb4ba5bb45a51
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-08-25 10:40:34 +0430

    success overlay - fix overlay, update overlay design

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,20 @@
         <router-view />
       </component>
     </transition>
+
+    <!-- overlay message -->
+    <transition name="fade">
+      <div
+        v-if="overlayModel.title || overlayModel.message"
+        class="w-full h-full fixed block top-0
+        left-0 z-110"
+      >
+        <BaseOverlaySuccess
+          :title="overlayModel.title"
+          :message="overlayModel.message"
+        />
+      </div>
+    </transition>
   </div>
 </template>
 <script>
