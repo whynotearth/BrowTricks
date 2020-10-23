97afa2182cd202cd8dbaa0aba6a9ea78b1df4af5
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-10-23 11:30:15 +0330

    splash - add splash screen for all pages

---
+++ src/components/splash/SplashOverlayContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fixed inset-0 z-100">
+    <div
+      class="bg-brand5 h-full flex flex-col justify-center items-center select-none"
+    >
+      <div class="max-w-xl w-full h-full">
+        <img
+          src="https://res.cloudinary.com/whynotearth/image/upload/v1598336510/BrowTricks/static_v2/brandcover_lo3i0l.jpg"
+          class="h-vh100 object-cover w-full"
+        />
+      </div>
+      <div class="absolute w-full h-full flex justify-center items-end pb-20">
+        <BaseSpinner borderColor="border-secondary" classNames="spinner" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseSpinner from '@/components/BaseSpinner.vue';
+import noPageScrollbarMixin from '@/utils/noPageScrollbarMixin';
+export default {
+  name: 'SplashOverlayContent',
+  // note: we use a mixin
+  mixins: [noPageScrollbarMixin],
+  components: { BaseSpinner }
+};
+</script>
