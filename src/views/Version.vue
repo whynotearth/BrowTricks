f2a351baac63931c2d4cd6f7cced2e2919b033e0
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-10-28 12:19:54 +0330

    version update

--- src/views/Version.vue
+++ src/views/Version.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="p-10">
-    <div v-if="BRANCH">
-      <p class="mb-8">Branch: {{ BRANCH }}</p>
-      <p class="mb-8">Review ID: {{ REVIEW_ID }}</p>
-      <p class="mb-8">Commit ref: {{ COMMIT_REF }}</p>
-      <p class="mb-8">Deploy Prime URL: {{ DEPLOY_PRIME_URL }}</p>
+    <p class="mb-1">Node environment: {{ NODE_ENV }}</p>
+    <div class="mb-8" v-if="CONTEXT">
+      <p class="mb-1">Review ID: {{ REVIEW_ID }}</p>
+      <p class="mb-1">Commit ref: {{ COMMIT_REF }}</p>
+      <p class="mb-1">Deploy Prime URL: {{ DEPLOY_PRIME_URL }}</p>
     </div>
-    <div v-else>
-      <p class="mb-8">Not available in local</p>
+    <div class="mb-8" v-else>
+      <p>Not available in local</p>
     </div>
 
     <p><router-link :to="{ name: 'Home' }">Home</router-link></p>
@@ -18,7 +18,8 @@
 export default {
   name: 'Version',
   data: () => ({
-    BRANCH: process.env.BRANCH,
+    CONTEXT: process.env.CONTEXT,
+    NODE_ENV: process.env.NODE_ENV,
     COMMIT_REF: process.env.COMMIT_REF,
     REVIEW_ID: process.env.REVIEW_ID,
     DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL
