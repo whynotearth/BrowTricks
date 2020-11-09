b6bb8da5f8683e42441fe604f7cb04317a361878
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-11-09 12:23:32 +0330

    ios - wip

--- src/components/CssSafeArea.vue
+++ src/components/CssSafeArea.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="safe-area-top" :class="{ isMobile, hasHeader }"></div>
+  <div>
+    <div class="safe-area-top" :class="{ isMobile, hasHeader }"></div>
+
+    <div class="debug"></div>
+    <div class="debug2"></div>
+  </div>
 </template>
 
 <script>
@@ -22,5 +27,17 @@ export default {
 }
 .hasHeader.safe-area-top {
   @apply bg-black;
+}
+.debug {
+  display: block;
+  width: 50px;
+  height: 100px;
+  background: red;
+}
+.debug2 {
+  display: block;
+  width: 50px;
+  height: env(safe-area-inset-top, 40px);
+  background: blue;
 }
 </style>
