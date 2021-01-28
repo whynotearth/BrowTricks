331204e2c078ea069fa80a72c16680fda826b620
Jayakarthikeyan G <jaya@infohawk.in>
2021-01-28 08:49:32 +0530

    feat(mobile): add third party login support

--- src/components/auth/AuthButtons.vue
+++ src/components/auth/AuthButtons.vue
@@ -13,7 +13,7 @@
             class="fill-current text-on-secondary text-opacity-high"
           />
         </template>
-        <template #end><i></i></template>
+        <template #end><em></em></template>
       </Button>
     </div>
   </div>
@@ -21,6 +21,7 @@
 
 <script>
 import { mapMutations, mapGetters } from 'vuex';
+const { cordova } = window;
 
 export default {
   name: 'AuthButtons',
@@ -38,6 +39,9 @@ export default {
         },
         {
           name: 'Google'
+        },
+        {
+          name: 'Apple'
         }
       ]
     };
@@ -61,7 +65,42 @@ export default {
     async openAuthentication(provider) {
       await this.updateProvider(provider);
       await this.updateReturnUrl(this.signupReturnUrl);
-      window.location.assign(this.oauthUrlGet);
+
+      if (process.env.VUE_APP_MOBILE && cordova) {
+        // When we run the App in mobile, then
+        // we will open the authentication page inside
+        // the mobile app.
+        const browser = cordova.InAppBrowser.open(
+          this.oauthUrlGet,
+          '_blank',
+          'location=yes'
+        );
+
+        // We will listen to load start page and find
+        // whether the query string has token in it.
+        browser.addEventListener(
+          'loadstart',
+          function(event) {
+            const currentURL = new URL(event.url);
+            const token = currentURL.searchParams.get('token');
+
+            // If we find a token then we assume the authentication
+            // process is completed.
+            if (token != null) {
+              const redirectURL = this.signupReturnUrl + '&token=' + token;
+
+              // Manually redirect to wait page to store the token
+              // and proceed further
+              window.location.assign(redirectURL);
+
+              // Close the browser we have opened.
+              browser.close();
+            }
+          }.bind(this)
+        );
+      } else {
+        window.location.assign(this.oauthUrlGet);
+      }
     }
   }
 };
