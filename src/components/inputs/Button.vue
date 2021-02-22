6f810751934f7600878a2b91163db5cfa56f2c68
Atharva Sharma <mailatharvasharma@gmail.com>
2021-02-22 10:26:21 +0530

    fix: hover on the button should not be there in case of no bg

--- src/components/inputs/Button.vue
+++ src/components/inputs/Button.vue
@@ -1,9 +1,12 @@
 <template>
   <component
     :is="type"
-    class="flex justify-center tg-color-label-mobile button cursor-pointer transition-all duration-75 select-none hover:shadow-lg"
     :class="[
-      isBordered ? 'border border-black' : '',
+      'flex justify-center tg-color-label-mobile button cursor-pointer transition-all duration-75 select-none',
+      {
+        'hover:shadow-lg': background !== null || isBordered,
+        'border border-black': isBordered
+      },
       shadow,
       margin,
       display,
